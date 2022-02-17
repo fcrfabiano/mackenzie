7197ad7ec12e488ba18a1a1754b248df8c6b7d14
Fabiano César <fcfabiano.cesar@gmail.com>
2022-02-17 11:40:39 -0300

    EXERCÍCIO 3 – Faça um programa em Python que calcule a distribuiçao de um premio em porcentagens

---
+++ aula-02-exercicios/exercicio3.py
@@ -0,0 +1,9 @@
+premio = 780000
+
+ganhador1 = premio * 0.46
+ganhador2 = premio * 0.32
+ganhador3 = premio * 0.22
+
+print(f'Ganhador 1 {ganhador1}')
+print(f'Ganhador 2 {ganhador2}')
+print(f'Ganhador 3 {ganhador3}')
